5c57f0a05b622e87004ec58cec71dc09af8fbbdf
LenyPython <buniadyserv@gmail.com>
2021-04-10 19:03:54 +0200

    made kv file and procticed on it

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from book.LoginScreen import LoginScreen
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import NumericProperty
+
+class LoginScreen(GridLayout):
+	cols = NumericProperty(2)
 
 class AddrBook(App):
 	def build(self):
