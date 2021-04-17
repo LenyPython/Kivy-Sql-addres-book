41c37fc2cb67c644aff84a50d4283815fcf45e9e
LenyPython <buniadyserv@gmail.com>
2021-04-17 18:16:43 +0200

    working on a simple sqlite db

---
+++ db_connect/sql_connect.py
@@ -0,0 +1,83 @@
+import sqlite3
+from sqlite3 import Error
+
+path = r'sql_db/my_db.db'
+create_user= '''CREATE TABLE IF NOT EXISTS users (
+					id integer PRIMARY KEY,
+					user text NOT NULL,
+					password text NOT NULL
+					);'''
+
+create_contacts='''CREATE TABLE IF NOT EXISTS contacts (
+					id integer PRIMARY KEY,
+					name text NOT NULL,
+					second_name text NOT NULL,
+					email text NOT NULL,
+					phone integer
+					);'''
+def connect(path):
+	'''Create a db connection to the SQLite database
+		specified by the path
+		:param path: database file
+		:return: Connectin object or None
+	'''
+	connection = None
+	try:
+		connection = sqlite3.connect(path)
+		print('Connection succesfull')
+		return connection
+	except Error as e:
+		print(f'Error: {e} occuerd')
+
+def close_connection(conn):
+	'''Close given connection
+	:param conn: Connection object
+	:return: 
+	'''
+	try:
+		conn.close()
+		print('Connection was closed')
+	except Error as e:
+		print(f'Error: {e} occuerd')
+
+def create_table(conn, query):
+	'''create a table from the query statment
+	:param conn: Connection object
+	:param query: a CREATE TABLE statment
+	:return:
+	'''
+	try:
+		cursor = conn.cursor()
+		cursor.execute(query)
+	except Error as e:
+		print(f'Error: {e} occured')
+
+def do_query(conn, query):
+	'''Execute query statment
+	:param conn: Connection object
+	:param query: query statment
+	:return:
+	'''
+	cursor = conn.cursor()
+	try:
+		cursor.execute(query)
+		conn.commit()
+		print('Query succes!')
+	except Error as e:
+		print(f'Error: {e} occuerd')
+
+def read_query(conn, query):
+	'''Read query form data base
+	:param conn: Connection object
+	:param query: query statment
+	:return result: data fetched from data base
+	'''
+	cursor = conn.cursor()
+	result = None
+	try:
+		cursor.execute(query)
+		result = cursor.fetchall()
+		return result
+	except Error as e:
+		print(f'Error: {e} occuerd')
+
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from db_connect.sql_connect import connect, do_query, read_query, close_connection, path
+from db_connect.sql_connect import create_table, create_user, create_contacts
 
 class LoginScreen(Widget):
 	def register(self):
-		print('reg')
+		connection = connect(path)
+		create_table(connection, create_user)
+		create_table(connection, create_contacts)
+
+		close_connection(connection)
 
 	def sing_in(self, user_id, password, *args):
 		print(f'User: {user_id}, password: {password}')
+		connection = connect(path)
 
 class AddrBook(App):
 	def build(self):
